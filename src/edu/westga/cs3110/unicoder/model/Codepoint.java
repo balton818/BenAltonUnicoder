55174162b42ebc52392c51f24fd9e04c9efcf6fe
balton818 <balton1@my.westga.edu>
2021-09-16 21:54:59 -0400

    Stubbed out private methods for toUTF8()

--- src/edu/westga/cs3110/unicoder/model/Codepoint.java
+++ src/edu/westga/cs3110/unicoder/model/Codepoint.java
@@ -1,21 +1,53 @@
 package edu.westga.cs3110.unicoder.model;
 
 public class Codepoint {
+	
+	private String hex;
 
 	public Codepoint(String hexadecimal) {
-		 System.out.println("Test commit");
+		if (hexadecimal == null) {
+			throw new IllegalArgumentException("Hexadecimal string cannot be null");
+		}
+		if (hexadecimal.isEmpty()) {
+			throw new IllegalArgumentException("Hexadecimal string cannot be empty");
+		}
+		this.hex = hexadecimal;
 	}
-	
+
 	public String toUTF32() {
 		return null;
 	}
-	
+
 	public String toUTF16() {
 		return null;
 	}
-	
+
 	public String toUTF8() {
+		long hexAsInt = Integer.parseUnsignedInt(this.hex, 16);
+		if (hexAsInt >= 0b0 && hexAsInt < 0b10000000) {
+			this.encodeAsSingleByteUTF8();
+		} else if (hexAsInt >= 0b10000000 && hexAsInt < 0b100000000000) {
+			this.encodeAsTwoByteUTF8();
+		} else if (hexAsInt >= 0b100000000000 && hexAsInt < 0b1111111111111111) {
+			this.encodeAsThreeByteUTF8();
+		}
+		
 		return null;
 	}
-	
+
+	private void encodeAsThreeByteUTF8() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void encodeAsTwoByteUTF8() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void encodeAsSingleByteUTF8() {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
