6548f2a39e0cd5e2fc2bf2094396cff7929fbd91
Ben Alton <balton1@my.westga.edu>
2021-09-16 20:44:40 -0400

    initial commit

---
+++ src/edu/westga/cs3110/unicoder/model/Codepoint.java
@@ -0,0 +1,21 @@
+package edu.westga.cs3110.unicoder.model;
+
+public class Codepoint {
+
+	public Codepoint(String hexadecimal) {
+		
+	}
+	
+	public String toUTF32() {
+		return null;
+	}
+	
+	public String toUTF16() {
+		return null;
+	}
+	
+	public String toUTF8() {
+		return null;
+	}
+	
+}
