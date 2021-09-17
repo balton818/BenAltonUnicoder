55ffaf2766b22f02f4be8218ccb4144877f5e471
balton818 <balton1@my.westga.edu>
2021-09-16 21:14:09 -0400

    test commit to check GitHub integration

--- src/edu/westga/cs3110/unicoder/model/Codepoint.java
+++ src/edu/westga/cs3110/unicoder/model/Codepoint.java
@@ -3,7 +3,7 @@ package edu.westga.cs3110.unicoder.model;
 public class Codepoint {
 
 	public Codepoint(String hexadecimal) {
-		 
+		 System.out.println("Test commit");
 	}
 	
 	public String toUTF32() {
