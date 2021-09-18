a04a0d7a8c273189a631b3e5f90e0eeb50b7544f
balton818 <balton1@my.westga.edu>
2021-09-17 20:03:22 -0400

    Added helper code to utf8(), started testing

--- src/edu/westga/cs3110/unicoder/model/Codepoint.java
+++ src/edu/westga/cs3110/unicoder/model/Codepoint.java
@@ -23,31 +23,43 @@ public class Codepoint {
 	}
 
 	public String toUTF8() {
-		long hexAsInt = Integer.parseUnsignedInt(this.hex, 16);
+		int hexAsInt = Integer.parseUnsignedInt(this.hex, 16);
+		
+		
 		if (hexAsInt >= 0b0 && hexAsInt < 0b10000000) {
-			this.encodeAsSingleByteUTF8();
+			return this.encodeAsSingleByteUTF8(hexAsInt);
 		} else if (hexAsInt >= 0b10000000 && hexAsInt < 0b100000000000) {
-			this.encodeAsTwoByteUTF8();
+			return this.encodeAsTwoByteUTF8(hexAsInt);
 		} else if (hexAsInt >= 0b100000000000 && hexAsInt < 0b1111111111111111) {
-			this.encodeAsThreeByteUTF8();
+			return this.encodeAsThreeByteUTF8(hexAsInt);
+		} else {
+			throw new IllegalArgumentException("Hex value is out of range for UTF8 encoding");
 		}
+	}
+
+	private String encodeAsThreeByteUTF8(int hexAsInt) {
 		
 		return null;
 	}
 
-	private void encodeAsThreeByteUTF8() {
-		// TODO Auto-generated method stub
-		
+	private String encodeAsTwoByteUTF8(int hexAsInt) {
+		return null;
 	}
 
-	private void encodeAsTwoByteUTF8() {
-		// TODO Auto-generated method stub
+	private String encodeAsSingleByteUTF8(int hexAsInt) {
+		String firstHexDigit = "";
+		String secondHexDigit = "";
+		String result = "00";
+		String bits = String.format("%8s", hexAsInt).replace(" ", "0");
+		System.out.println(bits);
 		
-	}
-
-	private void encodeAsSingleByteUTF8() {
-		// TODO Auto-generated method stub
+		firstHexDigit = bits.substring(0, 4);
+		secondHexDigit = bits.substring(5, 8);
+		result += firstHexDigit;
+		result += secondHexDigit;
+		System.out.println(result);
 		
+		return result;
 	}
 
 }
---
+++ src/edu/westga/cs3110/unicoder/tests/model/codepoint/TestToUtf8.java
@@ -0,0 +1,17 @@
+package edu.westga.cs3110.unicoder.tests.model.codepoint;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs3110.unicoder.model.Codepoint;
+
+class TestToUtf8 {
+
+	@Test
+	void testSingleByte() {
+		Codepoint character = new Codepoint("00AE");
+		
+	}
+
+}
